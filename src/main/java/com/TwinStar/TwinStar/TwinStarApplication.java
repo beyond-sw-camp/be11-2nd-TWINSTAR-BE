2c6cb63b0e8add31823ecb684175dd1bf7d6c73f
jaeseok <wotjrdl98@naver.com>
2025-01-24 18:55:03 +0900

    first

---
+++ src/main/java/com/TwinStar/TwinStar/TwinStarApplication.java
@@ -0,0 +1,13 @@
+package com.TwinStar.TwinStar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwinStarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwinStarApplication.class, args);
+	}
+
+}
