a841e309d653a9ca97335b72ffd7b7a1adebbc9a
ki-hyun-Jang <skjwj0511@gmail.com>
2025-02-08 14:40:35 +0900

    [feat] 컨트롤러에서 home화면 게시물 리스트보기 기능추가

--- src/main/java/com/TwinStar/TwinStar/post/controller/PostReadController.java
+++ src/main/java/com/TwinStar/TwinStar/post/controller/PostReadController.java
@@ -1,10 +1,27 @@
 package com.TwinStar.TwinStar.post.controller;
 
-import org.springframework.stereotype.Controller;
+import com.TwinStar.TwinStar.post.dto.HomePostResDto;
+import com.TwinStar.TwinStar.post.service.PostReadService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("post")
 public class PostReadController {
 
+    private final PostReadService postReadService;
 
+    public PostReadController(PostReadService postReadService) {
+        this.postReadService = postReadService;
+    }
+
+    @GetMapping("/home")
+    public ResponseEntity<?> getHomePost(@RequestParam Long userId) {
+        return ResponseEntity.ok(postReadService.getHomePost(userId));
+    }
 }
