e8359a787eadc3b59329617ea46437c4e993a8b4
ki-hyun-Jang <skjwj0511@gmail.com>
2025-02-08 15:27:09 +0900

    [feat] 컨트롤러에서 home화면 게시물 리스트보기 수정2

--- src/main/java/com/TwinStar/TwinStar/post/service/PostReadService.java
+++ src/main/java/com/TwinStar/TwinStar/post/service/PostReadService.java
@@ -1,7 +1,18 @@
 package com.TwinStar.TwinStar.post.service;
 
+import com.TwinStar.TwinStar.post.domain.Post;
+import com.TwinStar.TwinStar.post.dto.HomePostResDto;
 import com.TwinStar.TwinStar.post.repository.PostRepository;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PostReadService {
@@ -12,6 +23,8 @@ public class PostReadService {
         this.postRepository = postRepository;
     }
 
-    public Object getHomePost(Long userId) {
+    //    home화면에서 post 리스트 보여주는 메서드
+    public Page<HomePostResDto> getHomePost(Long userId) {
+        return null;
     }
 }
