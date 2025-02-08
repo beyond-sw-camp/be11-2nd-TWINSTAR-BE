6d4808ec8d7abaf67e00218b8af922b1f2234619
ki-hyun-Jang <skjwj0511@gmail.com>
2025-02-08 13:42:11 +0900

    [feat] ProfilePostResDto 추가

--- src/main/java/com/TwinStar/TwinStar/post/controller/PostReadController.java
+++ src/main/java/com/TwinStar/TwinStar/post/controller/PostReadController.java
@@ -1,7 +1,10 @@
 package com.TwinStar.TwinStar.post.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class PostReadController {
+
+
 }
