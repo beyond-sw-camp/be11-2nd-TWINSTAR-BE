57486b7d9b7620d408dd68b36d749205db010685
KiHyeon Jang <kihyeon_jang@KiHyeonui-MacBookPro.local>
2025-03-16 21:17:59 +0900

    deploy test

--- src/main/java/com/TwinStar/TwinStar/common/config/SecurityConfig.java
+++ src/main/java/com/TwinStar/TwinStar/common/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
 
     private CorsConfigurationSource corsConfiguration() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // 프론트엔드 도메인
+        configuration.setAllowedOrigins(List.of("http://localhost:3000","https://www.alexandrelax.store")); // 프론트엔드 도메인
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메서드
         configuration.setAllowedHeaders(List.of("*")); // 모든 헤더 허용
         configuration.setAllowCredentials(true); // 쿠키, 인증 정보 포함 허용
